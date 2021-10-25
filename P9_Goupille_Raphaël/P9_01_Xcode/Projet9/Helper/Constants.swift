53179d089ed8dc7db2d2a245226597c1a2451673
Raphael Goupille <raphaelgoupille@gmail.com>
2021-10-25 15:43:19 +0200

    Projet9 init

---
+++ P9_Goupille_Raphaël/P9_01_Xcode/Projet9/Helper/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Projet9
+//
+//  Created by Raphaël Goupille on 14/09/2021.
+//
+
+import Foundation
+
+class Constants {
+    static let firstCurrencyKey = ""
+    static let secondCurrencyKey = ""
+    
+    static let translateKey = ""
+    
+    static let weatherKey = ""
+}
